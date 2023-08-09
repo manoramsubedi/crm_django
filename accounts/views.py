926dbadc1a3c1b5297ac79cd5ba6cd34d031d9c6
manoramsubedi <manoramsubedi7@gmail.com>
2023-08-09 20:39:41 +0545

    db model added

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . models import *
+
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    content = {'orders': orders, 'customers' : customers }
+    #orderStatus = 
+    return render(request, 'accounts/dashboard.html',content)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    # we  can pass it into template by throwing in dictionary
+    return render(request, 'accounts/products.html', {'products':products})
+    
 
 def customers(request):
     return render(request, 'accounts/customers.html')
